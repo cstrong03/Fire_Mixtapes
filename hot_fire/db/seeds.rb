0153a98e9541e2224ee09ef78aa68360a3cff3b3
Charisse Strong <msstrong92@gmail.com>
2019-06-04 16:02:06 -0400

    relationship setup

--- hot_fire/db/seeds.rb
+++ hot_fire/db/seeds.rb
@@ -2,6 +2,17 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+# <img src={music.album}/>
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user1 = User.create(first_name: "Monica", username: "magic", email: "hey@fakemail.com")
+user2 = User.create(first_name: "Lori", username: "vegan_goddess", email: "heyyou@fakemail.com")
+user3 = User.create(first_name: "Fluffy", username: "cottonball", email: "tears@fakemail.com")
+user4 = User.create(first_name: "Niecey", username: "barbie", email: "ratchet@fakemail.com")
+
+track1 = Track.create(song_title: "Crazy in Love", artist: "Beyoncé", album: "Dangerously in Love")
+track2 = Track.create(song_title: "Super Bass", artist: "Nicki Minaj", album: "Pink")
+track3 = Track.create(song_title: "Never Be Like You", artist: "Flume ft Kai", album: "Skin")
+track4 = Track.create(song_title: "Hotline Bling", artist: "Drake", album: "Views")
