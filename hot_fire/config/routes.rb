7fc3a000c83c1d814b55b25985005577af7088ed
Charisse Strong <msstrong92@gmail.com>
2019-06-07 15:09:20 -0400

    cleaned up

--- hot_fire/config/routes.rb
+++ hot_fire/config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-
-  get 'upload/new'
-  get 'upload/create'
-  get 'upload/index'
   resources :tracks
-  resources :uploads
+
 
   scope '/user' do
     resources :tracks
